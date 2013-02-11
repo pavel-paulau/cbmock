47af89af1d6381912f079d7c7f204a43e54bf377
pavel-paulau <pavel.paulau@gmail.com>
2013-02-11 14:33:39 -0800

    release new 0.2.2 version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cbmock',
-    version='0.1',
+    version='0.2.2',
     description='Couchbase mock server',
     author='Pavel Paulau',
     author_email='pavel.paulau@gmail.com',
