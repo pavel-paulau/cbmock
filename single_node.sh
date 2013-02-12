2eec67f065ba1145130b7a8bacab021cec92d1c8
pavel-paulau <pavel.paulau@gmail.com>
2013-02-12 15:31:42 -0800

    use silent mode in shell scripts

--- single_node.sh
+++ single_node.sh
@@ -1,17 +1,17 @@
-curl -F "path=/pools/default" -F "method=GET" -F "response_code=200" \
+curl -s -F "path=/pools/default" -F "method=GET" -F "response_code=200" \
     -F "response_body=@samples/pools_default.json" 127.0.0.1:8080 > /dev/null
 
-curl -F "path=/pools/default/buckets" -F "method=GET" -F "response_code=200" \
+curl -s -F "path=/pools/default/buckets" -F "method=GET" -F "response_code=200" \
     -F "response_body=@samples/pools_default_buckets.json" 127.0.0.1:8080 > /dev/null
 
-curl -F "path=/pools/default/buckets/default/nodes" -F "method=GET" -F "response_code=200" \
+curl -s -F "path=/pools/default/buckets/default/nodes" -F "method=GET" -F "response_code=200" \
     -F "response_body=@samples/pools_default_buckets_default_nodes.json" 127.0.0.1:8080 > /dev/null
 
-curl -F "path=/pools/default/buckets/default/stats" -F "method=GET" -F "response_code=200" \
+curl -s -F "path=/pools/default/buckets/default/stats" -F "method=GET" -F "response_code=200" \
     -F "response_body=@samples/pools_default_buckets_default_stats.json" 127.0.0.1:8080 > /dev/null
 
-curl -F "path=/pools/default/buckets/default/statsDirectory" -F "method=GET" -F "response_code=200" \
+curl -s -F "path=/pools/default/buckets/default/statsDirectory" -F "method=GET" -F "response_code=200" \
     -F "response_body=@samples/pools_default_buckets_default_statsDirectory.json" 127.0.0.1:8080 > /dev/null
 
-curl -F "path=/pools/default/buckets/default/nodes/127.0.0.1%3A8091/stats" -F "method=GET" -F "response_code=200" \
+curl -s -F "path=/pools/default/buckets/default/nodes/127.0.0.1%3A8091/stats" -F "method=GET" -F "response_code=200" \
     -F "response_body=@samples/pools_default_buckets_default_nodes_127.0.0.1_stats.json" 127.0.0.1:8080 > /dev/null
