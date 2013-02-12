37cc634eaca472a91d29c7b54ac2da9e8f1e3021
pavel-paulau <pavel.paulau@gmail.com>
2013-02-12 15:36:28 -0800

    improve readability of shell scripts

--- single_node.sh
+++ single_node.sh
@@ -1,17 +1,23 @@
-curl -s -F "path=/pools/default" -F "method=GET" -F "response_code=200" \
-    -F "response_body=@samples/pools_default.json" 127.0.0.1:8080 > /dev/null
+curl -s -F "path=/pools/default" \
+        -F "response_body=@samples/pools_default.json" \
+        -F "method=GET" -F "response_code=200" 127.0.0.1:8080 > /dev/null
 
-curl -s -F "path=/pools/default/buckets" -F "method=GET" -F "response_code=200" \
-    -F "response_body=@samples/pools_default_buckets.json" 127.0.0.1:8080 > /dev/null
+curl -s -F "path=/pools/default/buckets" \
+        -F "response_body=@samples/pools_default_buckets.json" \
+        -F "method=GET" -F "response_code=200" 127.0.0.1:8080 > /dev/null
 
-curl -s -F "path=/pools/default/buckets/default/nodes" -F "method=GET" -F "response_code=200" \
-    -F "response_body=@samples/pools_default_buckets_default_nodes.json" 127.0.0.1:8080 > /dev/null
+curl -s -F "path=/pools/default/buckets/default/nodes" \
+        -F "response_body=@samples/pools_default_buckets_default_nodes.json" \
+        -F "method=GET" -F "response_code=200" 127.0.0.1:8080 > /dev/null
 
-curl -s -F "path=/pools/default/buckets/default/stats" -F "method=GET" -F "response_code=200" \
-    -F "response_body=@samples/pools_default_buckets_default_stats.json" 127.0.0.1:8080 > /dev/null
+curl -s -F "path=/pools/default/buckets/default/stats" \
+        -F "response_body=@samples/pools_default_buckets_default_stats.json" \
+        -F "method=GET" -F "response_code=200" 127.0.0.1:8080 > /dev/null
 
-curl -s -F "path=/pools/default/buckets/default/statsDirectory" -F "method=GET" -F "response_code=200" \
-    -F "response_body=@samples/pools_default_buckets_default_statsDirectory.json" 127.0.0.1:8080 > /dev/null
+curl -s -F "path=/pools/default/buckets/default/statsDirectory" \
+        -F "response_body=@samples/pools_default_buckets_default_statsDirectory.json" \
+        -F "method=GET" -F "response_code=200" 127.0.0.1:8080 > /dev/null
 
-curl -s -F "path=/pools/default/buckets/default/nodes/127.0.0.1%3A8091/stats" -F "method=GET" -F "response_code=200" \
-    -F "response_body=@samples/pools_default_buckets_default_nodes_127.0.0.1_stats.json" 127.0.0.1:8080 > /dev/null
+curl -s -F "path=/pools/default/buckets/default/nodes/127.0.0.1%3A8091/stats" \
+        -F "response_body=@samples/pools_default_buckets_default_nodes_127.0.0.1_stats.json" \
+        -F "method=GET" -F "response_code=200" 127.0.0.1:8080 > /dev/null
